5be32aca9ca5fbcd5a3dafc69f7cdb4c708e52c3
Alvyn Abranches <alvynabranches@gmail.com>
2020-10-17 20:22:21 +0530

    17102020v1

--- go/02_Inputs/hello.go
+++ go/02_Inputs/hello.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	var myString string
-	fmt.Scanln(&myString)
+	// var myString string
+	// fmt.Scanln(&myString)
+	var name string = "alvyn"
+	var a, _ = fmt.Println(name)
+	fmt.Println(a)
+
+	reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your full name: ")
+	// myname, _ := reader.ReadString('\n')
+	// fmt.Println(myname)
+
+	fmt.Print("Enter your rating: ")
+	myrating, _ := reader.ReadString('\n')
+	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(myrating), 64)
+	fmt.Println(mynumRating)
 }
